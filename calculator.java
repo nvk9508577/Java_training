ecba44a7fc121690fe16c51995c77a75b84a0e86
nvk9508577 <84273639+nvk9508577@users.noreply.github.com>
2023-04-17 21:59:31 +0530

    Update calculator.java

--- calculator.java
+++ calculator.java
@@ -6,14 +6,11 @@ class Main {
     char operator;
     Double number1, number2, result;
 
-    // create an object of Scanner class
     Scanner input = new Scanner(System.in);
 
-    // ask users to enter operator
     System.out.println("Choose an operator: +, -, *, or /");
     operator = input.next().charAt(0);
 
-    // ask users to enter numbers
     System.out.println("Enter first number");
     number1 = input.nextDouble();
 
@@ -22,25 +19,21 @@ class Main {
 
     switch (operator) {
 
-      // performs addition between numbers
       case '+':
         result = number1 + number2;
         System.out.println(number1 + " + " + number2 + " = " + result);
         break;
 
-      // performs subtraction between numbers
       case '-':
         result = number1 - number2;
         System.out.println(number1 + " - " + number2 + " = " + result);
         break;
-
-      // performs multiplication between numbers
+      
       case '*':
         result = number1 * number2;
         System.out.println(number1 + " * " + number2 + " = " + result);
         break;
 
-      // performs division between numbers
       case '/':
         result = number1 / number2;
         System.out.println(number1 + " / " + number2 + " = " + result);
